82a2c4b2a5502070723024b6f72e4574b182f98e
Shubham <shubhamsutariya21k@gmail.com>
2025-02-14 11:26:24 +0530

    chec(workflow + tsconfig):github ci workflow yml file update + front end tsconfig file updated to import the depandancy with name alias

--- apps/front-end/eslint.config.mjs
+++ apps/front-end/eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
   recommendedConfig: js.configs.recommended,
 });
 
-export default [
+const eslintConfig = [
   ...fixupConfigRules(compat.extends('next')),
   ...fixupConfigRules(compat.extends('next/core-web-vitals')),
   ...baseConfig,
@@ -19,3 +19,5 @@ export default [
     ignores: ['.next/**/*'],
   },
 ];
+
+export default eslintConfig;
