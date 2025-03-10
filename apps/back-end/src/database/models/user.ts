2dc17df5c92785dab150ca56765665a0626b640f
Om <transcode.om@gmail.com>
2025-03-10 15:41:56 +0530

    feat(user): create apis operations for users module

--- apps/back-end/src/database/models/user.ts
+++ apps/back-end/src/database/models/user.ts
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 import { IRole, IUser } from "@agent-xenon/interfaces";
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, index: true, },
-    password: { type: String, required: true },
+    firstName: { type: String, },
+    lastName: { type: String, },
+    email: { type: String, index: true, },
+    password: { type: String, },
     organizationId: { type: mongoose.Schema.Types.ObjectId, default: "Organization" },
     roleId: { type: mongoose.Schema.Types.ObjectId, default: null, ref: "role" },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, default: null, ref: "user" },
     deletedAt: { type: Date, default: null, },
 }, { timestamps: true, versionKey: false })
 
