6358b54d4ba9d4eb14213b38a57a5a1861e6d0e7
Om <transcode.om@gmail.com>
2025-03-18 15:59:53 +0530

    fix(interview question): question add, update validation updated, remove unused fields in get question by id api

--- apps/back-end/src/validation/question-answer.ts
+++ apps/back-end/src/validation/question-answer.ts
@@ -1,14 +1,16 @@
 import Joi from "joi";
 import { paginationSchema } from "./pagination";
-import { AnswerQuestionFormat, AnswerMcqOptionFormat, Difficulty } from "@agent-xenon/constants";
+import { AnswerQuestionFormat, AnswerMcqOptionFormat, Difficulty, InterviewRoundTypes } from "@agent-xenon/constants";
 
 const questionAnswerSchema = {
-    description: Joi.string().optional(),
+    description: Joi.string().allow("").optional(),
     options: Joi.array().items({ text: Joi.string().required(), isRightAnswer: Joi.boolean().required(), index: Joi.string().valid(AnswerMcqOptionFormat.A, AnswerMcqOptionFormat.B, AnswerMcqOptionFormat.C, AnswerMcqOptionFormat.D).required() }).optional(),
     tags: Joi.array().items(Joi.string()).optional(),
     difficulty: Joi.string().valid(Difficulty.EASY, Difficulty.MEDIUM, Difficulty.HARD).optional(),
     timeLimitInMinutes: Joi.number().optional(),
-    evaluationCriteria: Joi.string().optional(),
+    evaluationCriteria: Joi.string().allow("").optional(),
+    type: Joi.string().valid(...Object.values(InterviewRoundTypes)).optional(),
+    isAutomated: Joi.boolean().optional(),
     questionFormat: Joi.string().valid(AnswerQuestionFormat.MCQ, AnswerQuestionFormat.CODE, AnswerQuestionFormat.FILE, AnswerQuestionFormat.TEXT).optional(),
 }
 
