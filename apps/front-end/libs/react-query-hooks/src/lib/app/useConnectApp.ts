02b4589ad0fd7f204c4281182531a4ac0706b492
Shubham <shubhamsutariya21k@gmail.com>
2025-03-22 18:08:29 +0530

    fix(apps): app connect and disconnect logic implement

--- apps/front-end/libs/react-query-hooks/src/lib/app/useConnectApp.ts
+++ apps/front-end/libs/react-query-hooks/src/lib/app/useConnectApp.ts
@@ -2,9 +2,10 @@ import { useMutation } from '@tanstack/react-query';
 import { IApiResponse } from '@agent-xenon/interfaces';
 import { connectApp } from '@agent-xenon/web-apis';
 import { AxiosError } from 'axios';
+import { IConnectAppRequest, IConnectAppResponse } from '@/libs/types-api/src/lib/app';
 
-export const useGetConnectApp = () => {
-  return useMutation<IApiResponse<string>, AxiosError, string>({
+export const useConnectApp = () => {
+  return useMutation<IApiResponse<IConnectAppResponse>, AxiosError, IConnectAppRequest>({
     mutationKey: ['useGetConnectApp'],
     mutationFn: (params) => {
       return connectApp(params);
--- apps/front-end/libs/react-query-hooks/src/lib/app/useDisconnectApp.ts
+++ apps/front-end/libs/react-query-hooks/src/lib/app/useDisconnectApp.ts
@@ -2,9 +2,10 @@ import { useMutation } from '@tanstack/react-query';
 import { IApiResponse } from '@agent-xenon/interfaces';
 import { disconnectApp } from '@agent-xenon/web-apis';
 import { AxiosError } from 'axios';
+import { IDisConnectAppRequest } from '@/libs/types-api/src/lib/app';
 
-export const useGetDisconnectApp = () => {
-  return useMutation<IApiResponse<string>, AxiosError, string>({
+export const useDisconnectApp = () => {
+  return useMutation<IApiResponse<string>, AxiosError, IDisConnectAppRequest>({
     mutationKey: ['useGetDisconnectApp'],
     mutationFn: (params) => {
       return disconnectApp(params);
