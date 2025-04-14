8d9f6a4bd1ea2f405789ccd8371dfe226795670b
dixita <transcode.dixita@gmail.com>
2025-04-15 01:45:21 +0530

    feat(Employee): Create employee UI and pages with API integration

---
+++ apps/front-end/libs/react-query-hooks/src/lib/employees/index.ts
@@ -0,0 +1,5 @@
+export * from './useGetEmployees'
+export * from './useCreateEmployee'
+export * from './useDeleteEmployee'
+export * from './useGetEmployeeById'
+export * from './useUpdateEmployee'
